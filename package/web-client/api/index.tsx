6cfc096ce8f92015e4ed990f7f79c8f483ce847b
unknown <nour.edaher.jouini@superverse.tech>
2023-04-03 10:59:14 +0100

    add get page by url

--- package/web-client/api/index.tsx
+++ package/web-client/api/index.tsx
@@ -1,2 +1,3 @@
 export * from "./useAddReportedUrl";
 export * from "./useReportedPages";
+export * from "./usePageByUrl";
--- package/web-client/src/contentScript/contentScript.tsx
+++ package/web-client/src/contentScript/contentScript.tsx
@@ -3,35 +3,43 @@ import "../index.css";
 import PolygonAlert from "./polygon-alert";
 import { Button } from "../components/ui/button";
 import CheckIcon from "./check";
-import { useReportedPages } from "../../api";
+import { useReportedPages, usePageByUrl } from "../../api";
 
 const ContentScript = () => {
+  const url = window.location.origin.replace(/^https?:\/\//, "");
+  const [pageUrl, setPageUrl] = React.useState(url);
   const { data, isSuccess, refetch, isLoading, isFetching } =
-    useReportedPages();
+    usePageByUrl(pageUrl);
+
+  console.log("pageUrl", data);
 
   return (
-    <div className="cs-container">
-      <div className="background-popup-container">
-        <PolygonAlert />
-        <div className="container-title">
-          Sentinel has <span>detected</span> this as a phishing website!
+    <>
+      {data && (
+        <div className="cs-container">
+          <div className="background-popup-container">
+            <PolygonAlert />
+            <div className="container-title">
+              Sentinel has <span>detected</span> this as a phishing website!
+            </div>
+            <div className="btn-container">
+              <Button
+                title={"Visit the real website"}
+                variant={"sentinel-primary"}
+                rounded={false}
+              >
+                <CheckIcon />
+              </Button>
+              <Button
+                title={"Continue Anyways"}
+                variant={"sentinel-outline"}
+                rounded={false}
+              />
+            </div>
+          </div>
         </div>
-        <div className="btn-container">
-          <Button
-            title={"Visit the real website"}
-            variant={"sentinel-primary"}
-            rounded={false}
-          >
-            <CheckIcon />
-          </Button>
-          <Button
-            title={"Continue Anyways"}
-            variant={"sentinel-outline"}
-            rounded={false}
-          />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
